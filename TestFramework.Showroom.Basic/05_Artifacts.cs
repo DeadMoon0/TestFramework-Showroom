e6fc42da7c11b4fdd906361ed4ebe05aeace5d67
DeadMoon0 <94380754+DeadMoon0@users.noreply.github.com>
2026-05-01 14:54:06 +0200

    reduce and fix gaps

--- TestFramework.Showroom.Basic/05_Artifacts.cs
+++ TestFramework.Showroom.Basic/05_Artifacts.cs
@@ -5,6 +5,16 @@ using Xunit.Abstractions;
 
 namespace TestFramework.Showroom.Basic;
 
+file static class ArtifactSampleTempDirectories
+{
+    public static string Create()
+    {
+        string path = Path.Combine(Path.GetTempPath(), $"showroom-basic-{Guid.NewGuid():N}");
+        Directory.CreateDirectory(path);
+        return path;
+    }
+}
+
 public class Artifacts_Setup(ITestOutputHelper outputHelper)
 {
     // Artifacts are created or used by a trigger that requires cleanup. And because I know you’ll forget sometimes ;) I will handle this. Keep adding them; I will keep cleaning them.
@@ -17,11 +27,21 @@ public class Artifacts_Setup(ITestOutputHelper outputHelper)
     [Fact]
     public async Task Run()
     {
-        var run = await this._timeline.SetupRun(outputHelper)
-            .AddVariable("cmdCommand", "Hello from an Artifact")
-            .AddFileArtifact("msgFile", "./msg.txt", "Hello from an Artifact") // Every artifact that needs to be set up must be added.
-            .RunAsync();
-        run.EnsureRanToCompletion();
+        string tempDir = ArtifactSampleTempDirectories.Create();
+        string artifactPath = Path.Combine(tempDir, "msg.txt");
+
+        try
+        {
+            var run = await this._timeline.SetupRun(outputHelper)
+                .AddVariable("cmdCommand", "Hello from an Artifact")
+                .AddFileArtifact("msgFile", artifactPath, "Hello from an Artifact") // Every artifact that needs to be set up must be added.
+                .RunAsync();
+            run.EnsureRanToCompletion();
+        }
+        finally
+        {
+            Directory.Delete(tempDir, true);
+        }
     }
 }
 
@@ -30,8 +50,8 @@ public class Artifacts_Register(ITestOutputHelper outputHelper)
     // If you have an artifact mid timeline-run created by an unexpected trigger - no worries. Just tell me and I’ll keep track.
 
     private readonly Timeline _timeline = Timeline.Create()
-        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdCreate")))
-        .RegisterArtifact("newFile", LocalIO.Artifacts.FileRef("outNew.txt"))
+        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdCreate"), Var.Ref<string>("cwd")))
+        .RegisterArtifact("newFile", LocalIO.Artifacts.FileRef(Var.Ref<string>("artifactPath")))
         //                           ^ An artifact reference is how I can identify your artifact among the wide expanse of data.
         .Trigger(Simple.Simple.Trigger.MessageBox(Var.Ref<string>("cmdShow")))
         .Build();
@@ -39,11 +59,23 @@ public class Artifacts_Register(ITestOutputHelper outputHelper)
     [Fact]
     public async Task Run()
     {
-        var run = await this._timeline.SetupRun(outputHelper)
-            .AddVariable("cmdCreate", "echo Hello from the new Artifact >> outNew.txt")
-            .AddVariable("cmdShow", "Hello from the new Artifact")
-            .RunAsync();
-        run.EnsureRanToCompletion();
+        string tempDir = ArtifactSampleTempDirectories.Create();
+        string artifactPath = Path.Combine(tempDir, "outNew.txt");
+
+        try
+        {
+            var run = await this._timeline.SetupRun(outputHelper)
+                .AddVariable("cmdCreate", "echo Hello from the new Artifact >> outNew.txt")
+                .AddVariable("cmdShow", "Hello from the new Artifact")
+                .AddVariable("cwd", tempDir)
+                .AddVariable("artifactPath", artifactPath)
+                .RunAsync();
+            run.EnsureRanToCompletion();
+        }
+        finally
+        {
+            Directory.Delete(tempDir, true);
+        }
     }
 }
 
@@ -52,20 +84,32 @@ public class Artifacts_Assert(ITestOutputHelper outputHelper)
     // Also, artifacts need to be assertable. There you go.
 
     private readonly Timeline _timeline = Timeline.Create()
-        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdCreate")))
-        .RegisterArtifact("newFile", LocalIO.Artifacts.FileRef("outAssert.txt"))
+        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdCreate"), Var.Ref<string>("cwd")))
+        .RegisterArtifact("newFile", LocalIO.Artifacts.FileRef(Var.Ref<string>("artifactPath")))
         .Build();
 
     [Fact]
     public async Task Run()
     {
-        var run = await this._timeline.SetupRun(outputHelper)
-            .AddVariable("cmdCreate", "echo Hello from the new Artifact >> outAssert.txt")
-            .RunAsync();
-        run.EnsureRanToCompletion();
+        string tempDir = ArtifactSampleTempDirectories.Create();
+        string artifactPath = Path.Combine(tempDir, "outAssert.txt");
 
-        Assert.Equal("Hello from the new Artifact \r\n", run.ArtifactStore.GetFileArtifact("newFile").Last.DataAsUtf8String);
-        //               ^ Just like variables, artifacts are stored in the ArtifactStore.
+        try
+        {
+            var run = await this._timeline.SetupRun(outputHelper)
+                .AddVariable("cmdCreate", "echo Hello from the new Artifact >> outAssert.txt")
+                .AddVariable("cwd", tempDir)
+                .AddVariable("artifactPath", artifactPath)
+                .RunAsync();
+            run.EnsureRanToCompletion();
+
+            Assert.Equal("Hello from the new Artifact \r\n", run.ArtifactStore.GetFileArtifact("newFile").Last.DataAsUtf8String);
+            //               ^ Just like variables, artifacts are stored in the ArtifactStore.
+        }
+        finally
+        {
+            Directory.Delete(tempDir, true);
+        }
     }
 }
 
@@ -74,22 +118,34 @@ public class Artifacts_Versions(ITestOutputHelper outputHelper)
     // It may happen that an artifact transforms and changes over the span of the timeline. This is no problem - just capture the artifact state at this moment.
 
     private readonly Timeline _timeline = Timeline.Create()
-        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdAppend")))
-        .RegisterArtifact("newFile", LocalIO.Artifacts.FileRef("outVersion.txt"))
-        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdAppend")))
+        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdAppend"), Var.Ref<string>("cwd")))
+        .RegisterArtifact("newFile", LocalIO.Artifacts.FileRef(Var.Ref<string>("artifactPath")))
+        .Trigger(LocalIO.Trigger.Cmd(Var.Ref<string>("cmdAppend"), Var.Ref<string>("cwd")))
         .CaptureArtifactVersion("newFile", "laterVersion")
         .Build();
 
     [Fact]
     public async Task Run()
     {
-        var run = await this._timeline.SetupRun(outputHelper)
-            .AddVariable("cmdAppend", "echo Some Log >> outVersion.txt")
-            .RunAsync();
-        run.EnsureRanToCompletion();
+        string tempDir = ArtifactSampleTempDirectories.Create();
+        string artifactPath = Path.Combine(tempDir, "outVersion.txt");
 
-        Assert.Equal("Some Log \r\n", run.ArtifactStore.GetFileArtifact("newFile").First.DataAsUtf8String);
-        Assert.Equal("Some Log \r\nSome Log \r\n", run.ArtifactStore.GetFileArtifact("newFile")["laterVersion"].DataAsUtf8String);
-        //                                                       ^ Not only via "first" or "last" can artifact versions be gathered - try just using the name.
+        try
+        {
+            var run = await this._timeline.SetupRun(outputHelper)
+                .AddVariable("cmdAppend", "echo Some Log >> outVersion.txt")
+                .AddVariable("cwd", tempDir)
+                .AddVariable("artifactPath", artifactPath)
+                .RunAsync();
+            run.EnsureRanToCompletion();
+
+            Assert.Equal("Some Log \r\n", run.ArtifactStore.GetFileArtifact("newFile").First.DataAsUtf8String);
+            Assert.Equal("Some Log \r\nSome Log \r\n", run.ArtifactStore.GetFileArtifact("newFile")["laterVersion"].DataAsUtf8String);
+            //                                                       ^ Not only via "first" or "last" can artifact versions be gathered - try just using the name.
+        }
+        finally
+        {
+            Directory.Delete(tempDir, true);
+        }
     }
 }
